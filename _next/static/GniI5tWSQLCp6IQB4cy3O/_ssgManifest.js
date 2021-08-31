d15a16fb95e9cd485eef43ddd35cd14219abd419
austinEng <213reeses@gmail.com>
2021-08-31 21:14:31 +0000

    Deploying to gh-pages from  @ f60d1c4c84a7d7d60be54eaad04583625e199e3b 🚀

---
+++ _next/static/GniI5tWSQLCp6IQB4cy3O/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F2021f\u002F[slug]","\u002F2021f\u002Fassignments\u002F[slug]","\u002F2021f\u002Fresources\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
